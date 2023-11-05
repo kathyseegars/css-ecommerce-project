a8889e50a1ce407171313099d2b50a74d03e3190
unknown <kathyseegars@hub.gov>
2023-11-05 23:55:21 +0300

    ecommerce project

---
+++ java/index.js
@@ -0,0 +1,8 @@
+/*const menuicon = document.getElementById("menu-icon");
+const menuicon = document.getElementById("menu");
+menuicon?.addEventListener("click",()=>{
+    if(menu.classname="hidden"){menu.classlist.remove(hidden);}
+if(menu.classname="hidden"){
+    menu.classlist.remove("hidden");
+}
+})*/
